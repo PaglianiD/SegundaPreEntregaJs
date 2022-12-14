7b82eab9004a6216bf2cff5a0ca6faf9d4e526dc
PaglianiD <diegopagliani10@gmail.com>
2022-12-13 22:09:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//PRODUCTOS DE NOVEDADES// 
+class novedades{
+  constructor (nombre, precio , id){
+  this.nombre = nombre;
+  this.precio = precio;
+  this.id = id;
+  }
+}
+
+const samsungs20fe = new novedades("samsung s20 fe",60000,1);
+const iphone14 = new novedades ("iphone 14 ","300000",2)
+const xiaomi12pro = new novedades("xiaomi 12 por",90000,3)
+const motoedge30por = new novedades("motorola edge 30 pre",120000,4)
+
+
